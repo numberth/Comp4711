1afd523e9f474681f2c712c45635b94de1622004
numberth <trista.huang@outlook.com>
2017-01-12 21:56:44 -0800

    Created Student class with basic functions

---
+++ Student.php
@@ -0,0 +1,19 @@
+<?php
+
+class Student {
+
+	function __construct() {
+	    $this->surname = '';
+	    $this->first_name = '';
+	    $this->emails = array();
+	    $this->grades = array();
+	}	
+
+	function add_email($which,$address) {
+	    $this->emails[$which] = $address;
+	}
+
+	function add_grade($grade) {
+	    $this->grades[] = $grade;
+	}
+}
